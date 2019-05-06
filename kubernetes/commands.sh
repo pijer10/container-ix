e9440963e39ae2ad6a8e66ebe1722967fd45c77f
Pijer10 <7195986+pijer10@users.noreply.github.com>
2019-05-06 20:54:23 +0200

    Update commands.sh

--- kubernetes/commands.sh
+++ kubernetes/commands.sh
@@ -2,8 +2,12 @@
 kubectl get nodes
 kubectl get nodes -o wide
 kubectl get pods --all-namespaces
+dokcer images
+docker ps
+watch kubectl get pods --all-namespaces
 
 
 ## WORKER NODES
 kubectl get nodes
 kubectl get pods
+watch kubectl get pods
